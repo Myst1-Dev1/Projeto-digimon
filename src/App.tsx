690a83fe94d055eff371ece009a6fd5df72aebc3
João Victor Vasconcelos <danteopzz@hotmail.com>
2022-11-29 13:24:36 -0300

    Projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import "./assets/styles/global.scss";
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/home';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Home />
   );
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { digiApi, GetDigimon } from "../../services/digiApi";
+import { GetDigimon } from "../../services/digiApi";
 import { getDigimons } from "../../types/getDigimons";
+import { MagnifyingGlass } from 'phosphor-react';
 import "./index.scss";
 
 export default function Home() {
@@ -14,13 +15,6 @@ export default function Home() {
     const startIndex = currentPage * itensPerPage;
     const endIndex = startIndex + itensPerPage;
     const currentItens = digimons.slice(startIndex, endIndex);
-
-    // useEffect(() => {
-    //   digiApi.get('/api/digimon')
-    //     .then(response => {
-    //         setDigimons(response.data);
-    //     })
-    // }, [])
 
     useEffect(() => {
         getAllDigimons()
@@ -43,7 +37,7 @@ export default function Home() {
     useEffect(() => {
         searchDigimon()
     }, [search])
-    
+
     useEffect(() => {
         setDigimons(digimon)
     }, [])
@@ -52,18 +46,20 @@ export default function Home() {
     return(
         <>
        
-            <div className="banner">
+            <div className="banner container-fluid">
                 <h1 className="text-center">DigiDex</h1>
-                <div className="searchBox d-flex flex-column justify-content-center align-items-center">
+                <div className="searchBox d-flex">
+                    <div className="search">
+                    <MagnifyingGlass className="text-light" size={30}/>
+                    </div>
                     <input type="text"
                     value={search}
                     onChange={(e:any) => setSearch(e.target.value)}
-                    placeholder="Pesquise por um digimon"/>
-                     {/* <button className="mt-4" onClick={searchDigimon}>Pesquisar</button> */}
+                    placeholder="Search"/>
                 </div>
             </div>
 
-            <div className="row py-5 text-light">
+            <div className="row px-5 py-5 text-light">
                 {currentItens.map((digis:any) => {
                     return (
                     <div key={digis.name} className="digi-card mb-5 d-flex justify-content-center flex-column align-items-center col-md-3 container py-5">
@@ -74,19 +70,22 @@ export default function Home() {
                                 <h6>Level</h6>
                                  <h6>{digis.level}</h6>
                              </div>                
-                            </div>
+                        </div>
                     </div>
                     )
                 })}
-                {/* <button onClick={() => {setPage(page + 1)}}>Próximo</button>
-                <button onClick={() => {setPage(page - 1)}}>Anterior</button> */}
+
                  <div className="d-flex justify-content-center pagination-digimons">
                     {Array.from(Array(pages), (item, index) => {
-                    return <button value={index} onClick={() => setCurrentPage(index)}>{index}</button> 
+                    return <button value={index} onClick={() => setCurrentPage(index)}>{index + 1}</button> 
                     })}
                 </div>
             </div>
 
+
+            <div className="end py-5">
+                <p>&copy; 2022 Feito por <a target="_blank" rel="noreferrer" href="https://myst1-dev.netlify.app/">Myst1 Dev</a> </p>
+            </div>
             
         </>
     )
